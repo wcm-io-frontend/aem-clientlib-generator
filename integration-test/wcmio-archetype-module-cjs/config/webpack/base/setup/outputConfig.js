b69e5213447b384de91a116c29710ddb543c5690
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-10-07 17:30:56 +0200

    Switch to ESM - update integration tests (#106)

---
+++ integration-test/wcmio-archetype-module-cjs/config/webpack/base/setup/outputConfig.js
@@ -0,0 +1,8 @@
+import path from "path";
+import { pathConfig } from "../../../path.config.js";
+
+export const output = {
+  path: path.resolve(pathConfig.paths.target),
+  filename: `${pathConfig.paths.jsTarget}/[name].js`,
+  chunkFilename: `${pathConfig.paths.jsTarget}/[name].[hash].js`
+};
---
+++ integration-test/wcmio-archetype-module-cjs/config/webpack/base/tasks/es6.js
@@ -0,0 +1,11 @@
+export const es6 = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
