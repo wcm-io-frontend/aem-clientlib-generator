dc2f8948f241a45303a60853eea4068831f82c66
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-10-07 17:16:01 +0200

    switch to esm config file

--- integration-test/wcmio-archetype-module/clientlib.config.js
+++ integration-test/wcmio-archetype-module/clientlib.config.js
@@ -1,4 +1,9 @@
-module.exports = {
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default {
   // default working directory (can be changed per 'cwd' in every asset option)
   context: __dirname,
 
